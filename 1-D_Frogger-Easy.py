ca05ccc80159ca9191b81a4313c80c56e4cde527
Alejandro Carrillo <carrilloalejandro0215@gmail.com>
2025-06-23 11:52:53 -0500

    Update 1-D_Frogger-Easy.py

--- 1-D_Frogger-Easy.py
+++ 1-D_Frogger-Easy.py
@@ -1,47 +1,77 @@
+# incorrect answer, dont know why
+
 squares,starting,goal = map(int,input().split())
 lista = []
 
 lista = list(map(int,input().split()))
 
-numeroActual = lista[starting-1]
+numeroEnLista = lista[starting-1]
+numeroEnListaAoperar = lista[starting-1]
+posicionEnLista = starting
+
+
 contador = 0 
 listaPosiciones = []
-lenghtLista = len(listaPosiciones)
+lenghtLista = len(lista)
 
-if (numeroActual == goal):
+
+if (numeroEnLista == goal):
     print("magic")
     print(0)
     exit(0)
 
-while numeroActual != goal:
-    if (numeroActual>=0):
-        PosicionActual = lista[starting-1]+starting
-        starting = PosicionActual
-        numeroActual = lista[starting-1]
-        flag = listaPosiciones.__contains__(PosicionActual)
+
+    
+
+while numeroEnLista != goal:
+
+
+
+    if (numeroEnListaAoperar>=0):
+        posicionEnLista = posicionEnLista + numeroEnListaAoperar
+        if(posicionEnLista<0):
+            print("left")
+            print(contador + 1)
+            exit(0)
+        if (posicionEnLista>lenghtLista):
+            print("right")
+            print(contador + 1)
+            exit(0)
+        numeroEnLista = lista[posicionEnLista-1]
+        
+        flag = listaPosiciones.__contains__(posicionEnLista)
         if (flag==True):
             contador = contador +1 
             print("cycle")
             print(contador)
             exit(0)
-        listaPosiciones.append(PosicionActual)
+        listaPosiciones.append(posicionEnLista)
         contador = contador +1
+        numeroEnListaAoperar = numeroEnLista
+        continue
 
-    if(numeroActual<0):
-        PosicionActual = starting - abs(lista[starting-1])
-        starting = PosicionActual
-        numeroActual = lista[starting-1]
-        flag = listaPosiciones.__contains__(PosicionActual)
+
+
+    if(numeroEnListaAoperar<0):
+        posicionEnLista = posicionEnLista - abs (numeroEnListaAoperar)
+        if(posicionEnLista<=0):
+            print("left")
+            print(contador + 1)
+            exit(0)
+        if (posicionEnLista>lenghtLista):
+            print("right")
+            print(contador + 1)
+            exit(0)
+        numeroEnLista = lista[posicionEnLista-1]
+        flag = listaPosiciones.__contains__(posicionEnLista)
         if (flag == True):
             contador = contador +1
             print("cycle")
             print(contador)
             exit(0)
-        listaPosiciones.append(PosicionActual)
+        listaPosiciones.append(posicionEnLista)
         contador = contador + 1
+        numeroEnListaAoperar = numeroEnLista
+        continue
 print("magic")
 print(contador)
-
-
-## the logic is good, now we are just missing what happens whne the frog goes left or right
-# maybe with a len or idk 
