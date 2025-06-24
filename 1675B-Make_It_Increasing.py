3010e5ebe508a16a449b37be981f8670facf3d23
Alejandro Carrillo <carrilloalejandro0215@gmail.com>
2025-06-23 20:54:52 -0500

    Update 1675B-Make_It_Increasing.py

--- 1675B-Make_It_Increasing.py
+++ 1675B-Make_It_Increasing.py
@@ -1,18 +1,39 @@
-lista = list(map(int,input().split()))
+cases = int(input())
 
-lenght = len(lista)
+flag= True
+listaResultados = []
 
-posicionMax = lenght-1
-posicionsiguiente = lenght-1-1
-print(posicionMax)
-contador = 0
+for k in range(cases):
+    n = input()
+    lista = list(map(int,input().split()))
+    flag = True
+    lenght = len(lista)
 
+    posicionMax = lenght-1
+    posicionsiguiente = lenght-1-1
+    contador = 0
 
-for i in range(lenght):
-    while(lista[posicionMax-1]>lista[posicionsiguiente]):
-        lista[posicionMax-1] = lista[posicionMax-1]//2
-        contador = contador + 1
-    posicionMax= posicionMax-1
+    if (len(lista)==1):
+        listaResultados.append(0)
+        continue
+    if (lista == sorted(lista, reverse=True)):
+        listaResultados.append(-1)
+        continue
 
-print(lista)
-print(contador)
+    while flag:
+        for i in range(lenght-1):
+            if (lista[i]>=lista[i+1]):
+                lista[i]= lista[i]//2
+                contador = contador +1
+        index = 0
+
+        for i in range(0,lenght):
+            if (lista[index]<lista[index+1]):
+                index = index+1
+                if (index==lenght-1):
+                    listaResultados.append(contador)
+                    flag=False
+                    break
+
+for j in listaResultados:
+    print(j)
