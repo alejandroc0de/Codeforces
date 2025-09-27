507b4aab91fabab00bfba71c41ca4677f87f32f4
Alejandro Carrillo <carrilloalejandro0215@gmail.com>
2025-09-26 20:16:47 -0500

    problem

--- 1213B-BadPrices.py
+++ 1213B-BadPrices.py
@@ -1,21 +1,25 @@
+#accepted (HLPD)
+
 datasets = int(input())
+
 resultados = []
 
 for _ in range(datasets):
     contador = 0 
-    price = int(input())
+    dias = int(input())
     prices = []
     prices = list(map(int, input().split(" ")))
+    min_price = float("inf")
 
-    prices.sort()
-
-    for i in prices:
-        if price > i:
+    for i in range(dias-1,-1,-1):
+        if(min_price<prices[i]):
             contador = contador + 1
-            continue
-        else:
-            continue
+        min_price = min(min_price, prices[i])
+
     resultados.append(contador)
 
-for _ in resultados:
-    print(_)
+
+for l in resultados:
+    print(l)
+
+
