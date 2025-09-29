cb295f52e1b4adf082921b79d7fe6e85269fcf9d
Alejandro Carrillo <carrilloalejandro0215@gmail.com>
2025-09-28 22:15:43 -0500

    problem 900

---
+++ 474A-Keyboard.py
@@ -0,0 +1,23 @@
+lista = []
+listaPalabra = []
+
+letras = "qwertyuiopasdfghjkl;zxcvbnm,./"
+
+for i in letras:
+    lista.append(i)
+
+side = input()
+palabra = input()
+
+
+if side == "R":
+    for z in palabra:
+        index = lista.index(z)
+        listaPalabra.append(lista[index-1])
+elif side == "L":
+    for z in palabra:
+        index = lista.index(z)
+        listaPalabra.append(lista[index+1])
+
+for x in listaPalabra:
+    print(x,end="")
